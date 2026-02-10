b49b584363b6b7cb6e8c40ec79dd04a22f84e9c5
csdavids519 <csdavids@proton.me>
2026-02-10 12:37:19 -0500

    removed textDisplay.tsx

--- src/pages/TypingLesson.tsx
+++ src/pages/TypingLesson.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import { useKeyboardLayout } from "../contexts/KeyboardLayoutContext";
 import KeyInput from "../components/KeyInputs";
-import TextDisplay from "../components/TextDisplay";
+// import TextDisplay from "../components/TextDisplay";
+import UserTest_0 from "../components/UserTest_0";
+import UserTest_1 from "../components/UserTest_1";
+import UserTest_2 from "../components/UserTest_2";
+import UserTest_3 from "../components/UserTest_3";
 
-function TypingLesson() {
+export default function TypingLesson() {
   const { topRow, homeRow, lowerRow } = useKeyboardLayout();
 
   // position tracking states
@@ -15,22 +19,80 @@ function TypingLesson() {
   console.log("cursorPosition: ", { cursorPosition });
   console.log("correctness: ", { correctness });
   console.log("displayText: ", { displayText });
+  const [userLevel, setUserLevel] = useState(0);
+
+  // Render appropriate test based on level
+  let renderTest;
+  const userMessage = `Level ${userLevel}`;
+
+  if (userLevel === 0) {
+    renderTest = (
+      <UserTest_0
+        topRow={topRow}
+        homeRow={homeRow}
+        lowerRow={lowerRow}
+        correctness={correctness}
+        setCorrectness={setCorrectness}
+        displayText={displayText}
+        setDisplayText={setDisplayText}
+        cursorPosition={cursorPosition}
+        setCursorPosition={setCursorPosition}
+        userLevel={userLevel}
+        setUserLevel={setUserLevel}
+      />
+    );
+  } else if (userLevel === 1) {
+    renderTest = (
+      <UserTest_1
+        topRow={topRow}
+        homeRow={homeRow}
+        lowerRow={lowerRow}
+        correctness={correctness}
+        setCorrectness={setCorrectness}
+        displayText={displayText}
+        setDisplayText={setDisplayText}
+        cursorPosition={cursorPosition}
+        setCursorPosition={setCursorPosition}
+        userLevel={userLevel}
+        setUserLevel={setUserLevel}
+      />
+    );
+  } else if (userLevel === 2) {
+    renderTest = (
+      <UserTest_2
+        correctness={correctness}
+        setCorrectness={setCorrectness}
+        displayText={displayText}
+        setDisplayText={setDisplayText}
+        cursorPosition={cursorPosition}
+        setCursorPosition={setCursorPosition}
+        userLevel={userLevel}
+        setUserLevel={setUserLevel}
+      />
+    );
+  } else if (userLevel === 3) {
+    renderTest = (
+      <UserTest_3
+        correctness={correctness}
+        setCorrectness={setCorrectness}
+        displayText={displayText}
+        setDisplayText={setDisplayText}
+        cursorPosition={cursorPosition}
+        setCursorPosition={setCursorPosition}
+        userLevel={userLevel}
+        setUserLevel={setUserLevel}
+      />
+    );
+  }
 
   return (
     <>
-      <h1 className="text-3xl font-bold">Welcome.</h1>
+      <h1 className="text-3xl font-bold">Ergo-Type.</h1>
       <div className="m-5 p-5 rounded-lg border-2 border-indigo-700">
-        <TextDisplay
-          correctness={correctness}
-          setCorrectness={setCorrectness}
-          displayText={displayText}
-          setDisplayText={setDisplayText}
-          cursorPosition={cursorPosition}
-          setCursorPosition={setCursorPosition}
-          topRow={topRow}
-          homeRow={homeRow}
-          lowerRow={lowerRow}
-        />
+        <>
+          <p>{userMessage}</p>
+          {renderTest}
+        </>
       </div>
       <KeyInput
         setCorrectness={setCorrectness}
@@ -41,5 +103,3 @@ function TypingLesson() {
     </>
   );
 }
-
-export default TypingLesson;
