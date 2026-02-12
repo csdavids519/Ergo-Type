fae4ef02151e39f51f9cdd83833ce7e44f365590
csdavids519 <csdavids@proton.me>
2026-02-11 22:38:04 -0500

    running on gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/Ergo-Type">
+    <BrowserRouter basename="/Ergo-Type/">
       <App />
     </BrowserRouter>
   </StrictMode>,
